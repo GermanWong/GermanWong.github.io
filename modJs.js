fbbe85d6f55c555b79368b39c8065e26a859820c
GermanTec <54922953+GermanWong@users.noreply.github.com>
2022-02-22 13:01:05 -0600

    Primera version
    Formularios

---
+++ modJs.js
@@ -0,0 +1,5 @@
+var hola = "Hola mundo"
+function procesarFormulario(){
+    document.getElementById("salida").innerHTML = "Pronto me pondre en contacto " + 
+    document.getElementById("fname").value + " "+document.getElementById("lname").value;
+}
